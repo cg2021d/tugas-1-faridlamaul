f13376f5c77c256e188decb78aed9e3122994261
faridlamaul <lamaulfarid71@gmail.com>
2021-12-03 03:13:50 +0700

    fix complete tugas 4

--- tugas4-realistic-material-reflection/main.js
+++ tugas4-realistic-material-reflection/main.js
@@ -1,74 +1,142 @@
-// import library
-import * as THREE from "./build/three.module.js";
-import { OrbitControls } from "./js/OrbitControls.js";
-import { FlakesTexture } from "./js/FlakesTexture.js";
-import { RGBELoader } from "./js/RGBELoader.js";
+import { OrbitControls } from "https://cdn.jsdelivr.net/npm/three@0.121.1/examples/jsm/controls/OrbitControls.js";
+import { GLTFLoader } from "https://threejsfundamentals.org/threejs/resources/threejs/r132/examples/jsm/loaders/GLTFLoader.js";
 
-let scene, camera, renderer, controls, pointlight;
+let camera, scene, renderer;
 
-function init() {
+let count = 0,
+	cubeCamera1,
+	cubeCamera2,
+	cubeRenderTarget1,
+	cubeRenderTarget2;
+
+(function init() {
 	scene = new THREE.Scene();
+	const color = 0x000000;
+	const near = 1;
+	const far = 100;
 
-	// add scene background
-	let urls = ["img/posx.jpg", "img/negx.jpg", "img/posy.jpg", "img/negy.jpg", "img/posz.jpg", "img/negz.jpg"];
-	let loader = new THREE.CubeTextureLoader();
-	scene.background = loader.load(urls);
+	// Panorama
+	const panorama = new THREE.CubeTextureLoader();
+	const textureBg = panorama.load([
+		"img/posx.jpg",
+		"img/negx.jpg",
+		"img/posy.jpg",
+		"img/negy.jpg",
+		"img/posz.jpg",
+		"img/negz.jpg",
+	]);
+	scene.background = textureBg;
+	scene.fog = new THREE.Fog(color, near, far);
 
-	renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
+	//lighting
+	const ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
+	scene.add(ambientLight);
+
+	const directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
+	directionalLight.position.set(24, 25, 60);
+	directionalLight.castShadow = true;
+	scene.add(directionalLight);
+
+	camera = new THREE.PerspectiveCamera(75, 2, 0.1, 100);
+	camera.position.x = 5;
+	camera.position.y = 5;
+	camera.position.z = 15;
+
+	const floorMaterial = new THREE.MeshPhongMaterial({
+		side: THREE.DoubleSide,
+		map: new THREE.TextureLoader().load("./img/lantai.jpg"),
+	});
+
+	const floor = new THREE.Mesh(new THREE.PlaneGeometry(50, 50, 100, 100), floorMaterial);
+	floor.receiveShadow = true;
+	floor.rotation.x = -Math.PI / 2;
+	floor.position.y = -3;
+	scene.add(floor);
+
+	const box1 = new GLTFLoader();
+	box1.load("model/wooden_crate.glb", (gltf) => {
+		gltf.scene.traverse(function (node) {
+			if (node.isMesh) {
+				node.castShadow = true;
+				node.receiveShadow = true;
+				node.position.set(-3, -0.9, -2);
+				node.scale.set(2, 2, 2);
+			}
+		});
+		scene.add(gltf.scene);
+	});
+
+	const box2 = new GLTFLoader();
+	box2.load("model/wooden_crate.glb", (gltf) => {
+		gltf.scene.traverse(function (node) {
+			if (node.isMesh) {
+				node.castShadow = true;
+				node.receiveShadow = true;
+				node.position.set(-2, -1.4, 5);
+				node.scale.set(1.5, 1.5, 1.5);
+			}
+		});
+		scene.add(gltf.scene);
+	});
+
+	cubeRenderTarget1 = new THREE.WebGLCubeRenderTarget(256, {
+		format: THREE.RGBFormat,
+		generateMipmaps: true,
+		minFilter: THREE.LinearMipmapLinearFilter,
+		encoding: THREE.sRGBEncoding,
+	});
+
+	cubeCamera1 = new THREE.CubeCamera(1, 1000, cubeRenderTarget1);
+
+	cubeRenderTarget2 = new THREE.WebGLCubeRenderTarget(256, {
+		format: THREE.RGBFormat,
+		generateMipmaps: true,
+		minFilter: THREE.LinearMipmapLinearFilter,
+		encoding: THREE.sRGBEncoding,
+	});
+
+	cubeCamera2 = new THREE.CubeCamera(1, 1000, cubeRenderTarget2);
+
+	const refGeometry = new THREE.SphereGeometry(1, 32, 32);
+	const refMaterial = new THREE.MeshBasicMaterial({
+		envMap: cubeRenderTarget2.texture,
+		combine: THREE.MultiplyOperation,
+		reflectivity: 1,
+	});
+	const reflective = new THREE.Mesh(refGeometry, refMaterial);
+
+	reflective.castShadow = true;
+	reflective.receiveShadow = true;
+
+	reflective.position.set(3, 2, -2);
+	scene.add(reflective);
+
+	renderer = new THREE.WebGLRenderer({ antialias: true });
+
+	const controls = new OrbitControls(camera, renderer.domElement);
+	controls.enableDamping = true;
+	controls.dampingFactor = 0.25;
+	controls.enableZoom = true;
+
 	renderer.setSize(window.innerWidth, window.innerHeight);
+	renderer.shadowMap.enabled = true;
+	renderer.render(scene, camera);
 	document.body.appendChild(renderer.domElement);
 
-	renderer.outputEncoding = THREE.sRGBEncoding;
-	renderer.toneMapping = THREE.ACESFilmicToneMapping;
-	renderer.toneMappingExposure = 1.25;
+	function animation() {
+		reflective.rotation.x += 0.01;
+		reflective.rotation.y += 0.01;
+		reflective.rotation.z += 0.01;
 
-	camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 1000);
-	camera.position.set(0, 0, 500);
-	controls = new OrbitControls(camera, renderer.domElement);
+		if (count % 2 === 0) {
+			cubeCamera1.update(renderer, scene);
+		} else {
+			cubeCamera2.update(renderer, scene);
+		}
 
-	// object move
-	controls.autoRotate = true;
-	controls.autoRotateSpeed = 0.5;
-	controls.enableDamping = true;
-
-	// lighting
-	pointlight = new THREE.PointLight(0xffffff, 1);
-	pointlight.position.set(200, 200, 200);
-	scene.add(pointlight);
-
-	let envmaploader = new THREE.PMREMGenerator(renderer);
-
-	new RGBELoader().setPath("textures/").load("small_harbor_01_4k.hdr", function (hdrmap) {
-		let envmap = envmaploader.fromCubemap(hdrmap);
-		let texture = new THREE.CanvasTexture(new FlakesTexture());
-		texture.wrapS = THREE.RepeatWrapping;
-		texture.wrapT = THREE.RepeatWrapping;
-		texture.repeat.x = 10;
-		texture.repeat.y = 6;
-
-		const ballMaterial = {
-			clearcoat: 1.0,
-			clearcoatRoughness: 0.1,
-			metalness: 0.9,
-			roughness: 0.5,
-			color: 0x847600,
-			normalMap: texture,
-			normalScale: new THREE.Vector2(0.15, 0.15),
-			envMap: envmap.texture,
-		};
-
-		let ballGeo = new THREE.SphereGeometry(100, 64, 64);
-		let ballMat = new THREE.MeshPhysicalMaterial(ballMaterial);
-		let ballMesh = new THREE.Mesh(ballGeo, ballMat);
-		scene.add(ballMesh);
-
-		animate();
-	});
-}
-// rendering loop
-function animate() {
-	controls.update();
-	renderer.render(scene, camera);
-	requestAnimationFrame(animate);
-}
-init();
+		count++;
+		renderer.render(scene, camera);
+		requestAnimationFrame(animation);
+	}
+	animation();
+})();
